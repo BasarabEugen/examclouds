eddb4df6c59f01fdfa059af6a068bd81a694844d
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-23 21:47:59 +0300

    lesson 2 finish

---
+++ src/lesson2/Lesson2.java
@@ -0,0 +1,25 @@
+package lesson2;
+/*
+ * Created by basar on 23.06.2018
+ */
+
+public class Lesson2 {
+        public static void main(String[] args) {
+//      single-line comment
+/*
+        multi-line comment
+ */
+        boolean b = false;
+        String s = "ABCD";
+        char c = 'C';
+        int i = 1;
+        float pi =  3.14f;
+        double d =  4.16;
+        System.out.println("boolean: " + b);
+        System.out.println("string: " + s);
+        System.out.println("char:" + c);
+        System.out.println("integer:" + i);
+        System.out.println("float: " + pi);
+        System.out.println("double: " + d);
+        }
+}
---
+++ src/lesson2/lesson2_2/sum.java
@@ -0,0 +1,13 @@
+package lesson2.lesson2_2;/*
+ * Created by basar on 23.06.2018
+ */
+
+public class sum {
+    public static int sumSum(){
+        int a = 4;
+        int b = 6;
+        int sum = a + b;
+
+        return sum;
+    }
+}
