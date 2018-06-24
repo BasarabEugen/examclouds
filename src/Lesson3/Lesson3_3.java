c1a9e1dbd28ffc302d0c2dd5dff680570ffda888
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-24 09:11:29 +0300

    lesson 3.3 finish

---
+++ src/Lesson3/Lesson3_3.java
@@ -0,0 +1,24 @@
+package Lesson3;/*
+ * Created by basar on 24.06.2018
+ * Создать программу, которая будет вычислять и выводить на экран сумму двух целых чисел, введённых пользователем.
+ * Если пользователь некорректно введёт хотя бы одно из чисел, то сообщать об ошибке.
+ */
+import java.util.Scanner;
+public class Lesson3_3 {
+    public static void main(String[] args) {
+        number();
+    }
+
+    private static void number(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an two integer numbers: ");
+        if (sc.hasNextInt()){
+            int i = sc.nextInt();
+            int e = sc.nextInt();
+            System.out.println(i + e);
+
+        } else {
+            System.out.println("You did not enter an integer");
+        }
+    }
+}
