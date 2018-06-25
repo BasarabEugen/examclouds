d95e818223e1ce0fa7dfe52b6c6c412b7571f750
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-25 16:16:16 +0300

    lesson 5.2

--- src/lesson5/Lesoon5_2.java
+++ src/lesson5/Lesoon5_2.java
@@ -1,6 +1,19 @@
 package lesson5;/*
  * Created by basar on 25.06.2018
+ * С помощью класса Scanner ввести целое число. Если это число от 0 до 10,
+ * вывести на консоль сообщение “Положительное число меньше 10”,
+ * иначе вывести сообщение “Положительное число больше 10 или отрицательное”.
+ * Используем оператор if-else
  */
-
+import java.util.*;
 public class Lesoon5_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if ( n < 10 && n > 0){
+            System.out.println("Положительное число меньше 10");
+        } else {
+            System.out.println("Положительное число больше 10 или отрицательное");
+        }
+     }
 }
