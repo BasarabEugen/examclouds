de7bc914058001ab5961761da5274db37e07661a
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-25 18:34:43 +0300

    lesson 5.7

---
+++ src/lesson5/Lesson_5_7.java
@@ -0,0 +1,13 @@
+package lesson5;/*
+ * Created by basar on 25.06.2018
+ * Распечатать 5 символов в одну строку начиная с ‘h’. Используем цикл for
+ */
+
+public class Lesson_5_7 {
+    public static void main(String[] args) {
+    char h;
+        for (int i = 104; i <= 108; i++) {
+            System.out.print((char) i);
+        }
+    }
+}
