c1295aa862768736cc5962de5509c4557b004499
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-25 15:48:31 +0300

    lesson 5.1 finish

---
+++ src/lesson5/Lesson5_1.java
@@ -0,0 +1,21 @@
+package lesson5;/*
+ * Created by basar on 25.06.2018
+ * Передать на вход программы число в качестве аргумента. Если оно нечетное, распечатать его. Используем оператор if.
+ * Используйте метод Integer.parseInt() для преобразования из String в int.
+ */
+
+import com.sun.org.apache.xerces.internal.parsers.IntegratedParserConfiguration;
+
+public class Lesson5_1 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        if (n % 2 != 0){
+            System.out.println("In the argument is not an even number "+ n);
+        } else {
+            System.out.println("In the argument an even number " + n);
+        }
+
+
+
+    }
+}
