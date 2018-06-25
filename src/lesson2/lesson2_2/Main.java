64bab7f677be6855695cd52a86bf851f6c49d38d
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-25 16:36:35 +0300

    lesson 2.2

---
+++ src/lesson2/lesson2_2/Main.java
@@ -0,0 +1,12 @@
+package lesson2.lesson2_2;
+/*
+ * Created by basar on 23.06.2018
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Sum s = new Sum();
+        System.out.println(Sum.sumSum());
+
+    }
+}
---
+++ src/lesson2/lesson2_2/Sum.java
@@ -0,0 +1,12 @@
+package lesson2.lesson2_2;/*
+ * Created by basar on 23.06.2018
+ */
+
+ class Sum {
+    static int sumSum(){
+        int a = 4;
+        int b = 6;
+
+        return a + b;
+    }
+}
