7551edbbd8ae4d667fea926d0669b4215742d26a
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-26 17:56:47 +0300

    lesson 5.9

---
+++ src/lesson5/Lesson5_9.java
@@ -0,0 +1,28 @@
+package lesson5;
+/*
+ * Created by basar on 25.06.2018
+ * Реализовать подсчет факториала используя цикл for.
+n! = 1*2*...*n;​
+0!=1;
+5! = 1*2*3*4*5;
+Число n задается случайным образом (используйте Math.random()).
+ */
+
+import java.util.Scanner;
+
+public class Lesson5_9 {
+
+     private static long factorial() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        long n = sc.nextInt();
+        long result = 1;
+        for (long factor = 1; factor <= n; factor++) {
+            result *= factor;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println("The factorial: " + factorial());
+    }
+}
