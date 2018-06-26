1472c4babd79d0c44a25d546ab8677df44bd5fc1
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-26 18:57:43 +0300

    lesson 5.10

---
+++ src/lesson5/Lesson5_10.java
@@ -0,0 +1,21 @@
+package lesson5;/*
+ * Created by basar on 25.06.2018
+ * Создать массив типа String с размером 7. Записать в него значения дней недели.
+ * Вывести на консоль значение последнего элемента.
+ */
+
+public class Lesson5_10 {
+    public static void main(String[] args) {
+
+        String[] week ={
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+        };
+        System.out.println(week[6]);
+    }
+}
