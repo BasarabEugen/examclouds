96885dd018218a6fd87e421b224d3c038c69b21b
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-26 19:11:33 +0300

    lesson 5.11

---
+++ src/lesson5/Lesson5_11.java
@@ -0,0 +1,13 @@
+package lesson5;/*
+ * Created by basar on 25.06.2018
+ * Создать массив типа double с размером 4.
+ * Записать в него любые значения с помощью блока для инициализации.
+ * Вывести на консоль значение первого элемента.
+ */
+
+public class Lesson5_11 {
+    public static void main(String[] args) {
+        double n = Double.parseDouble(args[0]);
+        System.out.println(n);
+    }
+}
