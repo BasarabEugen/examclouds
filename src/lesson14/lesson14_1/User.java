a3ae52adc9177839c6ec6d0e77fd594116a4f049
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-07-31 11:50:46 +0300

    lesson all

---
+++ src/lesson14/lesson14_1/User.java
@@ -0,0 +1,64 @@
+package lesson14.lesson14_1;/*
+ * Created by BASARAB EUGEN on 31.07.2018
+ */
+
+import java.util.Objects;
+
+public class User {
+    private String login;
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(login, user.login) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    void createQuery(){
+        Query query = new Query();
+        query.printToLog();
+    }
+
+    class Query{
+        void printToLog(){
+            System.out.println(User.this + " send query");
+        }
+    }
+}
